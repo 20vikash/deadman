f880dcaeab382d11ba3e1239c84af52c4133ea97
20vikash <thisisforspotifyvikash@gmail.com>
2026-06-04 13:10:25 +0000

    feat(deadman): Add notification Users DOCTYPE

--- deadman/deadman/doctype/deadman_settings/deadman_settings.py
+++ deadman/deadman/doctype/deadman_settings/deadman_settings.py
@@ -12,10 +12,18 @@ class DeadmanSettings(Document):
 	from typing import TYPE_CHECKING
 
 	if TYPE_CHECKING:
+		from deadman.deadman.doctype.notification_users.notification_users import NotificationUsers
 		from frappe.types import DF
 
 		deadman_token: DF.Password
 		default_grace_multiplier: DF.Float
+		notification_users: DF.Table[NotificationUsers]
+		telegram_bot_token: DF.Password | None
+		telegram_chat_id: DF.Data | None
+		twilio_account_sid: DF.Data | None
+		twilio_api_key_secret: DF.Password | None
+		twilio_api_key_sid: DF.Data | None
+		twilio_phone_number: DF.Data | None
 	# end: auto-generated types
 
 	_DOCTYPE_NAME = "Deadman Settings"
