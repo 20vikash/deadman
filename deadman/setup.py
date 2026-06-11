95ffa9923dd9c3728dfa74e4b6ec3b9650930e5d
20vikash <thisisforspotifyvikash@gmail.com>
2026-06-11 09:00:49 +0000

    feat(setup): Add default capabilities

--- deadman/setup.py
+++ deadman/setup.py
@@ -6,11 +6,35 @@ if TYPE_CHECKING:
 	from deadman.deadman.doctype.deadman_settings.deadman_settings import DeadmanSettings
 
 
-def initialize_setup(secret: str):
+def initialize_deadman(secret: str):
 	settings: DeadmanSettings = frappe.get_single("Deadman Settings")
 
 	settings.deadman_password = secret
-
 	settings.save(ignore_permissions=True)
+
+	default_capabilities = [
+		("incident_validation", 300),
+		("incident_resolution", 300),
+		("prometheus", 300),
+		("alertmanager", 300),
+		("twilio", 600),
+	]
+
+	for capability_name, heartbeat_interval in default_capabilities:
+		if frappe.db.exists(
+			"Capability",
+			{
+				"capability_name": capability_name,
+			},
+		):
+			continue
+
+		frappe.get_doc(
+			{
+				"doctype": "Capability",
+				"capability_name": capability_name,
+				"heartbeat_interval": heartbeat_interval,
+			}
+		).insert(ignore_permissions=True)
 
 	frappe.db.commit()
